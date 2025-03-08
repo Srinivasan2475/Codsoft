8d36885cad263412bd2adc65ef0b159109893196
srinivasan V <23it56@ksriet.ac.in>
2025-03-08 14:20:22 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,71 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var typingEffect = new Typed(".typedText", {
+        strings: [ "Web Developer", "Programmer","Graphic Designer"],
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 2000,
+        loop: true
+    });
+});
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+ const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true     
+ })
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
+
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+  origin: 'right',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+
+
+
+
